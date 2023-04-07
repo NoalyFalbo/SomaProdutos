cb954c2d3b6a4eea39342a3022b7b044a9d32fd7
Noaly Falbo <noalyfalbos@outlook.com>
2023-04-07 16:08:00 -0300

    commit no testes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+//sing System.Globalization;
+
+namespace MeuCodigo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      // Console.Clear();
+      //Console.WriteLine("Digite o codigo da peça : ");
+      //Console.ReadLine();
+      Console.WriteLine("Digite a quantidade de peça : ");
+      Console.WriteLine("Digite o valor unitario da peça: ");
+
+      double quantidade, valorUnitario, total, pagar;
+
+      valorUnitario = double.Parse(Console.ReadLine());
+      quantidade = double.Parse(Console.ReadLine());
+      total = valorUnitario * quantidade;
+      pagar = total;
+
+      Console.WriteLine("Valor a pagar R$ " + pagar);
+
+    }
+  }
+}
